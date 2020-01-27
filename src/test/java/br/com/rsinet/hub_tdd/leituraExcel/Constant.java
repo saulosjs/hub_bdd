1510c28f2ba23a8ea50bf0ef7b192f7132dcab78
saulo jose da silva <saulo.silva@rsinet.com.br>
2020-01-27 14:30:16 -0300

    arrumando o caminho do excel

--- src/test/java/br/com/rsinet/hub_tdd/leituraExcel/Constant.java
+++ src/test/java/br/com/rsinet/hub_tdd/leituraExcel/Constant.java
@@ -3,7 +3,7 @@ package br.com.rsinet.hub_tdd.leituraExcel;
 public class Constant {
 
 	public static final String URL = "http://www.advantageonlineshopping.com/#/";
-	public static final String Path_TestData = "C:\\Users\\saulo.silva\\Downloads\\hub_TDD_BDD-master\\hub_TDD_BDD-master\\src\\main\\java\\arquivos\\";
+	public static final String Path_TestData = "target/";
 	public static final String File_TestData = "bancoDados.xlsx";
 
 }
